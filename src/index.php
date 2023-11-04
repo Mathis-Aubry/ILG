8abfa8807eaf16986a951a510712c74b94941302
Mathis-Aubry <mathisaubry.me@gmail.com>
2023-11-04 17:56:58 +0100

    more wrap adjustments and style

--- src/index.php
+++ src/index.php
@@ -7,8 +7,15 @@
         <title>ILG</title>
     </head>
     <body>
-        <pre></pre>
-        <textarea autocomplete="off" spellcheck="false"></textarea>
+        <!-- In this <pre> tag is where the text is really highlighted, because we can't do it in the textarea directly.
+        At any time the text written in the textarea must be there as well, perfectly aligned for the illusion not to break.
+        Also, the overflow, the spaces and newlines must be managed the exact same way as in the textarea.
+        If not done properly there will be a missalignment of the textarea's caret,
+        and the user won't be able to click and edit where he wants. -->
+        <div>
+            <pre></pre>
+            <textarea autocomplete="off" spellcheck="false"></textarea>
+        </div>
         <button>Run</button>
         <script src="script/main.js?<?=time();?>"></script>
     </body>
